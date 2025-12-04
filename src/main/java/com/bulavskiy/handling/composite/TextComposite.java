2e95ccc463614529c1d5d8dcffb2f17b314679fc
andrei1910bl <as006405@g.bstu.by>
2025-12-04 20:19:15 +0300

    fix

--- src/main/java/com/bulavskiy/handling/composite/TextComposite.java
+++ src/main/java/com/bulavskiy/handling/composite/TextComposite.java
@@ -3,7 +3,10 @@ package com.bulavskiy.handling.composite;
 import java.util.ArrayList;
 import java.util.List;
 
-public class TextComposite implements TextComponent{
+public class TextComposite implements TextComponent {
+
+  private static final String PARAGRAPH_SEPARATOR = "\n\t";
+  private static final String WORD_SEPARATOR = " ";
 
   private List<TextComponent> components = new ArrayList<>();
   private TextType type;
@@ -34,15 +37,13 @@ public class TextComposite implements TextComponent{
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
-    for(TextComponent component : components){
+    for (TextComponent component : components) {
       sb.append(component.toString());
 
       if (type == TextType.TEXT) {
-        sb.append("\n\t");
-      } else if (type == TextType.PARAGRAPH) {
-        sb.append(" ");
-      } else if (type == TextType.SENTENCE) {
-        sb.append(" ");
+        sb.append(PARAGRAPH_SEPARATOR);
+      } else if (type == TextType.PARAGRAPH || type == TextType.SENTENCE) {
+        sb.append(WORD_SEPARATOR);
       }
     }
     return sb.toString();
@@ -50,10 +51,10 @@ public class TextComposite implements TextComponent{
 
   @Override
   public int count() {
-    int a = 0;
+    int count = 0;
     for (TextComponent component : components) {
-      a += component.count();
+      count += component.count();
     }
-    return a;
+    return count;
   }
 }
